5b33d455b762e2b6234c857d58023b3d0d4e70ac
Chukak <chukak12@gmail.com>
2018-06-05 14:42:05 +0300

    added documentation to functions

--- src/queue.cpp
+++ src/queue.cpp
@@ -3,7 +3,11 @@
 #include <stdexcept>
 #include <new>
 
-
+/*
+ * Creates a new pointer.
+ * Copies a value from the another pointer to a new pointer.
+ * Returns a new pointer.
+ */
 static void *copy_value(const void *pointer)
 {
     void *p = ::operator new(sizeof(void *));
@@ -11,6 +15,10 @@ static void *copy_value(const void *pointer)
     return p;
 }
 
+/*
+ * Checks a pointer to the queue.
+ * If a pointer to the queue is NULL, Raises `runtime_error`.
+ */
 static void check_queue(const void *q) 
 {
     if (!q) {
@@ -18,6 +26,9 @@ static void check_queue(const void *q)
     }
 }
 
+/*
+ * Creates a queue and returns a pointer to it.
+ */
 queue *q_create_queue()
 {
     queue *q = new queue();
@@ -28,6 +39,9 @@ queue *q_create_queue()
     return q;
 }
 
+/*
+ * Removes the queue and all the elements from memory.
+ */
 void q_delete_queue(queue *q)
 {
     check_queue(q);
@@ -41,15 +55,20 @@ void q_delete_queue(queue *q)
     delete q;
 }
 
+/*
+ * Puts an element in the queue.
+ * Increases the size of the queue.
+ */
 void q_enqueue(queue *q, const void *element)
 {
     check_queue(q);
+    // if a pointer is NULL, raises `runtime_error`.
     if (!element) {
         throw std::runtime_error("The pointer to an element is NULL.");
     }
-    q_node *new_node = new q_node();
+    q_node *new_node = new q_node(); // a new node.
     new_node->next = NULL;
-    new_node->value = copy_value(element);
+    new_node->value = copy_value(element); // copies a value.
     if (q->empty) {
         q->qfront = new_node;
         q->qback = new_node;
@@ -61,6 +80,10 @@ void q_enqueue(queue *q, const void *element)
     q->count++;
 }
 
+/*
+ * Removes the last element from the queue and returns the deleted element.
+ * Reduces the size of the queue.
+ */
 void *q_dequeue(queue *q) 
 {
     check_queue(q);
@@ -68,7 +91,7 @@ void *q_dequeue(queue *q)
     void *value = NULL;
     if (!q->empty) {
         temp = q->qfront;
-        value = copy_value(q->qfront->value);
+        value = copy_value(q->qfront->value); // gets a value from the element.
         q->qfront = q->qfront->next;
         delete temp;
         q->count--;
@@ -80,7 +103,9 @@ void *q_dequeue(queue *q)
     return value;
 }
 
-
+/*
+ * Returns a front element from the queue.
+ */
 void *q_front(queue *q)
 {
     check_queue(q);
@@ -91,6 +116,9 @@ void *q_front(queue *q)
     return value;
 }
 
+/*
+ * Returns a back element from the queue.
+ */
 void *q_back(queue *q)
 {
     check_queue(q);
