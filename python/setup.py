4b46eb37926ff11fd2ca33f74d89cf33d44630a1
Chukak <chukak12@gmail.com>
2018-06-05 17:16:12 +0300

    updated script setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,7 @@
-#from distutils.core import setup, Extension
-from setuptools import  Extension, setup
-from setuptools.command.build_ext import build_ext
+import sys
+import os
+from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
 
 
 class BuildExt(build_ext):
@@ -9,22 +10,37 @@ class BuildExt(build_ext):
         super(BuildExt, self).build_extensions()
 
 
-setup(
-    cmdclass={"build_ext": BuildExt},
-    name="containers",
-    version="1.0",
-    description="",
-    url="https://github.com/Chukak/containers",
-    ext_modules=[
-        Extension(
-            "containers",
-            ["../src/queue.cpp", "pyqueue.c", "../src/stack.cpp", "pystack.c", "python.c", ],
-            include_dirs=["../src"],
-            extra_compile_args=[
-                "-g",  "-Wall", "-D_DEFAULT_SOURCE"
-            ],
-        ),
-    ],
-    py_modules=["containers"],
-)
-#"-Werror",
+if __name__ == "__main__":
+    one, two = sys.version_info[0], sys.version_info[1]
+    if os.environ.get("PY_HEADER_PATH", ""):
+        paths = [
+            str(os.environ["PY_HEADER_PATH"]),
+            "../src"
+        ]
+    else:
+        paths = [
+            "/usr/include/python" + str(one) + "." + str(two),
+            "/usr/local/include/python" + str(one) + "." + str(two),
+            "/usr/lib/python" + str(one) + "." + str(two),
+            "/usr/local/lib/python" + str(one) + "." + str(two),
+            "../src"
+        ]
+    os.environ["PY_HEADER_PATH"] = ""
+    setup(
+        cmdclass={"build_ext": BuildExt},
+        name="containers",
+        version="1.0",
+        description="",
+        url="https://github.com/Chukak/containers",
+        ext_modules=[
+            Extension(
+                "containers",
+                ["../src/queue.cpp", "pyqueue.c", "../src/stack.cpp", "pystack.c", "python.c", ],
+                include_dirs=paths,
+                extra_compile_args=[
+                    "-g", "-Werror", "-Wall"
+                ],
+            ),
+        ],
+        py_modules=["containers"],
+    )
