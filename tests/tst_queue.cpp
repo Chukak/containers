a5f8008b0bd4296ca8a157008bd43d10e7d76107
Chukak <chukak12@gmail.com>
2018-06-05 16:11:18 +0300

    fixed description of tests

--- tests/tst_queue.cpp
+++ tests/tst_queue.cpp
@@ -8,7 +8,7 @@ TEST_CASE("[Queue] Initialization an queue.", "[queue]") {
     /*
      * Testing the standart initialization.
      */
-    SECTION("Initialization testing №1.") {
+    SECTION("Testing the initialization №1.") {
         Queue<int> q; 
         
         REQUIRE(&q != nullptr); 
@@ -20,7 +20,7 @@ TEST_CASE("[Queue] Initialization an queue.", "[queue]") {
     /*
      * Testing the initialization `Queue q = {1,2,3}`.
      */
-    SECTION("Initialization testing №2.") {
+    SECTION("Testing the initialization №2.") {
         Queue<int> q = {1, 2, 3, 4, 5, 6};
         
         REQUIRE(&q != nullptr); 
@@ -34,7 +34,7 @@ TEST_CASE("[Queue] Initialization an queue.", "[queue]") {
     /*
      * Testing the initialization of the queue from another queue.
      */
-    SECTION("Initialization testing №3.") {
+    SECTION("Testing the initialization №3.") {
         Queue<int> orig = {1, 2, 3};
         REQUIRE(&orig != nullptr); 
         
@@ -54,8 +54,8 @@ TEST_CASE("[Queue] Initialization an queue.", "[queue]") {
 /*
  * Testing the insertion in the queue.
  */
-TEST_CASE("[Queue] Testing of insertion in the queue.", "[queue]") {
-    SECTION("Testing of insertion №1.") {
+TEST_CASE("[Queue] Testing the insertion in the queue.", "[queue]") {
+    SECTION("Testing the insertion №1.") {
         Queue<int> q;
         
         for (int i = 2; i < 520; i = i * 2) {
@@ -69,7 +69,7 @@ TEST_CASE("[Queue] Testing of insertion in the queue.", "[queue]") {
         
         q.~Queue();
     }
-    SECTION("Testing of insertion №2.") {
+    SECTION("Testing the insertion №2.") {
         Queue<int> q = {2, 4, 8};
         
         for (int i = q.back() * 2; i < 520; i = i * 2) {
@@ -88,8 +88,8 @@ TEST_CASE("[Queue] Testing of insertion in the queue.", "[queue]") {
 /*
  * Testing the removing from the queue.
  */
-TEST_CASE("[Queue] Testing of removing from the queue.", "[queue]") {
-    SECTION("Testing of removing №1.") {
+TEST_CASE("[Queue] Testing the removing from the queue.", "[queue]") {
+    SECTION("Testing the removing №1.") {
         Queue<int> q;
         
         for (int i = 2; i < 520; i = i * 2) {
@@ -108,7 +108,7 @@ TEST_CASE("[Queue] Testing of removing from the queue.", "[queue]") {
         
         q.~Queue();
     }
-    SECTION("Testing of removing №2.") {
+    SECTION("Testing the removing №2.") {
         Queue<int> q = {2, 4, 8};
         
         REQUIRE(q.dequeue() == 2);
@@ -136,8 +136,8 @@ TEST_CASE("[Queue] Testing of removing from the queue.", "[queue]") {
 /*
  * Testing the front and back elements from the queue.
  */
-TEST_CASE("[Queue] Testing of front and back elements from the queue.", "[queue]") {
-    SECTION("Testing of front element.") {
+TEST_CASE("[Queue] Testing the front and back elements from the queue.", "[queue]") {
+    SECTION("Testing the front element.") {
         Queue<int> q;
         
         for (int i = 2; i < 520; i = i * 2) {
@@ -162,7 +162,7 @@ TEST_CASE("[Queue] Testing of front and back elements from the queue.", "[queue]
         
         q.~Queue();
     }
-    SECTION("Testing of back element.") {
+    SECTION("Testing the back element.") {
         Queue<int> q;
         
         for (int i = 2; i < 520; i = i * 2) {
