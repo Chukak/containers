fda121bd210fc4183b4a1d5243dbeb4b713953df
Chukak <chukak12@gmail.com>
2018-06-05 13:04:37 +0300

    added documentation

--- src/queue.h
+++ src/queue.h
@@ -10,46 +10,100 @@
 #ifdef __cplusplus
 #include <initializer_list>
 
+/*
+ * The `Queue` class.
+ * The queue is the structure "First-In-First-Out". 
+ */
 template<typename Elem>
 class Queue {
 public:
+    /*
+     * A constructor.
+     */
     Queue();
+    /*
+     * A constructor, creates the `queue` class from the `queue` class.
+     * @param orig - a constant link to another `queue` class.
+     */
     Queue(const Queue<Elem> &orig);
+    /*
+     * A constructor, for the style `Queue q = {1, 2, 3}`.
+     */
     Queue(std::initializer_list<Elem> lst);
+    /*
+     * A destructor.
+     */
     virtual ~Queue();
     
+    /*
+     * An `enqueue` function.
+     * Puts an element into the queue.
+     * @param element - a constant link to the value.
+     */
     void enqueue(const Elem &element);
+    /*
+     * A `dequeue` funciton.
+     * Removes an element fron the queue. 
+     */
     Elem dequeue();
+    /*
+     * Returns the number of elements.
+     */
     unsigned int count() const { return qnum_of_elements; };
     
+    /*
+     * Returns the first element from the queue.
+     */
     Elem front() const;
+    /*
+     * Returns the last element from the queue.
+     */
     Elem back() const;
     
+    /*
+     * Returns `true` if the queue is empty, otherwise returns `false`. 
+     */
     bool empty() const { return qempty; }
 private:
+    /*
+     * A linked list structure.
+     * Used to represent elements in memory.
+     */
     struct Node {
-        Elem value;
-        Node *next;
+        Elem value; // a value.
+        Node *next; // a pointer to the next `Node` structure.
 
+        /*
+         * A constructor.
+         */
         Node(const Elem &v, Node *n) : value(v), next(n) {}
     };
     
-    Node *qfront;
-    Node *qback;
-    unsigned int qnum_of_elements;
+    Node *qfront; // a pointer to the first element.
+    Node *qback; // a pointer to the last element.
+    unsigned int qnum_of_elements; // the numbers of elements.
     bool qempty;
     
 };
 
+/*
+ * A costructor.
+ * Creates a new `Queue` class. 
+ */
 template<typename Elem>
 Queue<Elem>::Queue()
 {
-    qfront = NULL;
-    qback = NULL;
-    qnum_of_elements = 0;
-    qempty = true;
+    qfront = NULL; // a pointer to the first element
+    qback = NULL; // a pointer to the last element
+    qnum_of_elements = 0; // the numbers of elements
+    qempty = true; 
 }
 
+/*
+ * A constructor.
+ * Creates a new `Queue` class from the another `Qeueu` class.
+ * 
+ */
 template<typename Elem>
 Queue<Elem>::Queue(const Queue<Elem> &orig) : 
     qfront(NULL),
@@ -57,14 +111,17 @@ Queue<Elem>::Queue(const Queue<Elem> &orig) :
     qnum_of_elements(orig.qnum_of_elements),
     qempty(false)
 {
+    /*
+     * If an original class is empty, return from constructor.
+     */
     if (orig.empty()) {
         qempty = true;
-        return ;
+        return ; 
     }
-    Node *t = orig.qfront;
-    qfront = new Node(t->value, NULL);
+    Node *t = orig.qfront; // copy a pointer to the first element.
+    qfront = new Node(t->value, NULL); // creates a new pointer.
     qback = qfront;
-    t = t->next;
+    t = t->next; // gets a pointer to the next element.
     while (t) {
         Node *new_node = new Node(t->value, NULL);
         qback->next = new_node;
@@ -73,6 +130,9 @@ Queue<Elem>::Queue(const Queue<Elem> &orig) :
     } 
 }
 
+/*
+ * A constructor, for the style `Queue q = {1, 2, 3}`.
+ */
 template<typename Elem>
 Queue<Elem>::Queue(std::initializer_list<Elem> lst) :
     qfront(NULL), 
@@ -80,53 +140,77 @@ Queue<Elem>::Queue(std::initializer_list<Elem> lst) :
     qnum_of_elements(0),
     qempty(true)
 {
+    /*
+     * Just copy all the elements.
+     */
     for (auto element : lst) {
         enqueue(element);
     }
 }
 
+/*
+ * A destructor.
+ * Removes all the elements from memory.
+ */
 template<typename Elem>
 Queue<Elem>::~Queue() 
 {
     Node *old = NULL;
     while (qfront) {
-        old = qfront;
-        qfront = qfront->next;
+        old = qfront; // a pointer to a current element.
+        qfront = qfront->next; // a pointer to the next element.
         delete old;
     }
 }
 
+/*
+ * An `enqueue` function.
+ * Puts an element into the queue at the end.
+ * Increases the size of the queue.
+ */
 template<typename Elem>
 void Queue<Elem>::enqueue(const Elem &element) 
 {
-    Node *new_node = new Node(element, NULL);
-    if (qempty) {
-        qfront = new_node;
+    Node *new_node = new Node(element, NULL); // a new pointer to an element.
+    if (qempty) { 
+        qfront = new_node; // front == back.
         qback = new_node;
         qempty = false;
     } else {
-        qback->next = new_node;
-        qback = new_node;
+        qback->next = new_node; // sets the next element.
+        qback = new_node; // sets the last element.
     }
     qnum_of_elements++;
 }
 
+/*
+ * A `dequeue` function.
+ * Removes a front element from the queue.
+ * Reduces the size of the queue.
+ * If the queue is empty, the result has undefined behavior.
+ */
 template<typename Elem>
 Elem Queue<Elem>::dequeue() 
 {
     Node *temp = NULL;
-    Elem value;
+    Elem value; 
     if (!qempty) {
         temp = qfront;
         value = qfront->value;
-        qfront = qfront->next;
+        qfront = qfront->next; // sets a new front element.
         delete temp;
         qnum_of_elements--;
     }
-    qempty = qnum_of_elements == 0 ? true : false;
+    // checks if the queue is empty.
+    qempty = qnum_of_elements == 0 ? true : false; 
     return value;
 }
 
+/*
+ * A `front` function.
+ * Returns a front element from the queue.
+ * If the queue is empty, the result has undefined behavior.
+ */
 template<typename Elem>
 Elem Queue<Elem>::front() const
 {
@@ -137,6 +221,11 @@ Elem Queue<Elem>::front() const
     return value;
 }
 
+/*
+ * A `back` function.
+ * Returns a back element from the queue.
+ * If the queue is empty, the result has undefined behavior.
+ */
 template<typename Elem>
 Elem Queue<Elem>::back() const
 {
@@ -148,6 +237,9 @@ Elem Queue<Elem>::back() const
 }
 #endif /* __cplusplus */
 
+/*
+ * A declaration the structure `queue` for C.
+ */
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
@@ -158,28 +250,81 @@ extern "C" {
     
     typedef struct q_node q_node;
     
+    /*
+     * Node structure for the queue.
+     */
     struct q_node {
-        void *value;
-        q_node *next;
+        void *value; // a pointer to a value.
+        q_node *next; // a pointer to the next node.
     };
     
+    /*
+     * The structure of `queue`.
+     * The queue is the structure "First-In-First-Out". 
+     * 
+     */
     typedef struct {
-        q_node *qfront;
-        q_node *qback;
-        unsigned int count;
+        q_node *qfront; // a pointer to the first node.
+        q_node *qback; // a pointer to the last node.
+        unsigned int count; 
         int empty;
     } queue;
     
+    /*
+     * An `q_create_queue` function.
+     * Creates a queue and returns a pointer to it.
+     */
     queue *q_create_queue(void);
     
+    /*
+     * An `q_enqueue` function.
+     * Puts an element in the queue.
+     * Takes two arguments:
+     * `q` - a pointer to the queue.
+     * `element` - a pointer to the element.
+     */
     void q_enqueue(queue *q, const void *element);
+    /*
+     * An `q_dequeue` function.
+     * Removes an element in the queue. 
+     * Returns a pointer to the deleted element.
+     * If the queue is empty, returns the `NULL` pointer.
+     * Takes one arguments:
+     * `q` - a pointer to the queue.
+     */
     void *q_dequeue(queue *q);
     
+    /*
+     * An `q_front` function.
+     * Returns a pointer to the first element from the queue.
+     * If the queue is empty, returns the `NULL` pointer.
+     * Takes one arguments:
+     * `q` - a pointer to the queue.
+     */
     void *q_front(queue *q);
+    /*
+     * An `q_back` function.
+     * Returns a pointer to the last element from the queue.
+     * If the queue is empty, returns the `NULL` pointer.
+     * Takes one arguments:
+     * `q` - a pointer to the queue.
+     */
     void *q_back(queue *q);
     
+    /*
+     * An `q_count` function.
+     * Returns the size of the queue.
+     * Takes one arguments:
+     * `q` - a pointer to the queue.
+     * Use this function, if necessary.
+     * Or instead this function, use `queue->count`.
+     */
     unsigned int q_count(queue *q);
     
+    /*
+     * An `q_delete_queue` function.
+     * Removes the queue from memory.
+     */
     void q_delete_queue(queue *q);
 #ifdef __cplusplus
 }
