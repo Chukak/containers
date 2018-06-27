005775cc40c3f446d46048cf39e22b508af11e50
Chukak <chukak12@gmail.com>
2018-06-27 14:23:17 +0300

    created and updated the performance class for the sorted list

---
+++ tests/performance/sorted_list_perf.h
@@ -0,0 +1,105 @@
+#ifndef SORTED_LIST_PERF_H
+#define SORTED_LIST_PERF_H
+
+#include "../../src/sorted_list.h"
+#include "performance.h"
+#include <vector>
+
+class SListPerformance : public Performance {
+public:
+    explicit SListPerformance()
+    {}
+    
+private:
+    
+    void insertions(const uint& num)
+    {
+        sorted_list<int> list;
+        
+        srand(time(0));
+        
+        std::vector<int> random_elements;
+        
+        for (uint i = 0; i < num; i++) {
+            random_elements.push_back((rand() % num + 1) - (50000));
+        }
+        
+        start_timer();
+        for (uint s = 0; s < random_elements.size(); s++) {
+            list.push(random_elements[s]);
+        }
+        finish_timer();
+        
+        assert(list.count() == num);
+        
+        list.~sorted_list();
+    }
+    
+    void deletions(const uint& num)
+    {
+        sorted_list<int> list;
+        
+        for (uint i = 0; i < num; i++) {
+            list.push(i);
+        }
+        
+        assert(list.count() == num);
+        
+        srand(time(0));
+        
+        start_timer();
+        try {
+            for (uint i = 0; i != list.count();) {
+                list.remove(rand() % list.count());
+            } 
+        } catch (std::out_of_range& e) {
+            
+        }
+        finish_timer();
+        
+        list.~sorted_list();
+    }
+    
+    void clearing(const uint& num)
+    {
+        sorted_list<int> list;
+        
+        for (uint i = 0; i < num; i++) {
+            list.push(i);
+        }
+        
+        assert(list.count() == num);
+        
+        start_timer();
+        list.clear();
+        finish_timer();
+        
+        list.~sorted_list();
+    }
+    
+public:
+    
+    void run(ACTION action, const uint& number) 
+    {
+        reset();
+        switch (action) {
+            case ACTION::INSERTION:
+                insertions(number); break;
+            case ACTION::DELETION:
+                deletions(number); break;
+            case ACTION::CLEARING:
+                clearing(number); break;
+            default:
+                break;
+        }
+    }
+    
+    static const ACTION INSERTION = ACTION::INSERTION;
+    
+    static const ACTION DELETION = ACTION::DELETION;
+    
+    static const ACTION CLEARING = ACTION::CLEARING;
+};
+
+#endif /* SORTED_LIST_PERF_H */
+
