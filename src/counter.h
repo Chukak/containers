1d055e918f79b07c75080697b307a1ea82a59f77
Chukak <chukak12@gmail.com>
2018-08-24 22:37:43 +0300

    added commentary, added an overloaded operator `<<`

--- src/counter.h
+++ src/counter.h
@@ -16,18 +16,24 @@
 #include <utility>
 #include <algorithm>
 #include <iterator>
-#include <functional>
-#include <type_traits>
-#include <iostream>
+#include <ostream>
 
 
 using uint = unsigned int;
 
 
 /*
+ * The `Counter` class.
+ * Counts number of elements from a some sequence.
  */
 template<typename T>
 class Counter {
+    
+    /*
+     * Sets the friend function for the overloaded operator `<<`.
+     */
+    template<typename Type>
+    friend std::ostream& operator<<(std::ostream& stream, const Counter<Type>& c);
 
 public:
     
@@ -44,34 +50,71 @@ public:
     typedef std::pair<T, uint> pair_t;
     
     /*
+     * Constructor.
+     * @param _begin - the iterator to the beginning of the sequence.
+     * @param _end - the iterator to the end of the sequence. 
      */
     template<typename IteratorBegin, typename IteratorEnd>
     explicit Counter(IteratorBegin _begin, IteratorEnd _end);
     
     /*
+     * Destructor.
      */
     virtual ~Counter();
     
     /*
+     * The `most_common` function.
+     * Counts number of elements in the sequence.
+     * Return vector of pairs. The pairs contains two 
+     * values: (the element in the sequence, the number of such elements).
+     * For example: sequence - ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c') 
+     * -> {('a', 4), ('c', 3), ('b', 2), ('d', 1)}.
+     * The vector of pairs is sorted by the greater number of elements.
+     * If you want to get some number of elements, you must pass a 
+     * number as the first argument.
+     * @paran n - number.
      */
     std::vector<std::pair<T, uint>> most_common(int n = -1);
     
     /*
+     * The alternative `most_common` function.
+     * Counts number of elements in the sequence and
+     * sorts it by the greater number of elements.
+     * After it, this function writes result in the array, 
+     * which was passed as the first argument, but this function
+     * writes only elements in the sequence, without number of elements.
+     * For example: sequence - ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c') 
+     * -> {'a', 'c', 'b', 'd'}.
+     * Your array must have a type. For example: vector<T>.
+     * @param array - your array.
+     * @param n - number.
      */
     template<typename Array>
     void most_common(Array& list, int n = -1);
     
     /*
+     * Update the `Counter` class.
      */
     template<typename IteratorBegin, typename IteratorEnd>
     void update(IteratorBegin begin, IteratorEnd end);
     
     /*
+     * The static `map` function.
+     * Gets two iterators on the sequence.
+     * Countes number of eleemnts in the sequence and 
+     * sorts it by the greater number of elements.
+     * Return `std::map<T, int>`: T - element, int - number of this element.
+     * For example: ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c') 
+     * -> {'a': 4, 'c': 3, 'b': 2, 'd': 1}.
+     * @param _begin - the iterator to the beginning of the sequence.
+     * @param _end - the iterator to the end of the sequence. 
      */
     template<typename IteratorBegin, typename IteratorEnd>
     static std::map<T, uint> map(IteratorBegin _begin, IteratorEnd _end);
     
     /*
+     * The `size` function.
+     * Returns size of the original sequence.
      */
     uint size() const noexcept
     {
@@ -80,23 +123,32 @@ public:
     
 private:
     
+    /*
+     */
     using vc_it = typename std::vector<std::pair<T, uint>>::const_iterator;
     
     /*
+     * The private `count` function.
+     * Return pair of iterators on a sorted vector.
      */
     auto count(int& n) -> std::pair<vc_it, vc_it>;
     
     /*
+     * The private `update` function.
+     * Updates the vector of pairs and sorts it.
      */
     void update();
     
-    std::map<T, uint> _elements; //
-    std::vector<std::pair<T, uint>> _current; //
+    std::map<T, uint> _elements; // map of elements.
+    std::vector<std::pair<T, uint>> _current; // a sorted vector of elements.
     
 public:
     class iterator;
     
     /*
+     * The `iterator` class.
+     * Implements the iterator for the `Counter` class.
+     * The iterator is `forward_iterator`.
      */
     class iterator : public std::iterator<std::forward_iterator_tag, T> {
         
@@ -104,8 +156,9 @@ public:
         
     private:
         /*
+         * Constructor.
          */
-        explicit iterator(vc_it it) : _begin(it) 
+        explicit iterator(vc_it it, vc_it end) : _begin(it), _end(end)
         {}
         
     public:
@@ -115,6 +168,7 @@ public:
         typedef std::forward_iterator_tag iterator_category;
         
         /*
+         * Constrctor.
          */
         iterator() : _begin(NULL) {}
         
@@ -173,7 +227,7 @@ public:
          */
         bool operator!=(std::nullptr_t) const noexcept
         { 
-            return _begin != nullptr;
+            return _begin != _end;
         }
         
         /*
@@ -193,39 +247,59 @@ public:
          */
         bool operator==(std::nullptr_t) const noexcept
         { 
-            return _begin == nullptr;
+            return _begin == _end;
         }
         
     private:
-        vc_it _begin; //
-    
+        vc_it _begin; // the iterator of the beginning.
+        vc_it _end; // the iterator of the end.
     };
     
     /*
+     * The static `count` function.
+     * Counts number of elements in the sequence and
+     * sorts it by the greater number of elements.
+     * Return your array of pairs.
+     * Your array must supported by functions `std::transform` and `std::inserter`.
+     * For example: sequence - ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c')
+     * ```
+     * std::vector<Counter<std::string>::pair_t> vec = Counter<std::string>::count<std::vector>(begin(...), end(...));
+     * or use just 
+     * auto vec = Counter<std::string>::count<std::vector>(begin(...), end(...));
+     * or use another container
+     * std::set<Counter<std::string>::pair_t set = Counter<std::string>::count<std::set>(begin(...), end(...));
+     * ```
+     * @param _begin - the iterator to the beginning of the sequence.
+     * @param _end - the iterator to the end of the sequence. 
      */
     template<template <class ... > class Array,
             typename IteratorBegin, 
             typename IteratorEnd>
-    static Array<std::pair<T, uint>> count(IteratorBegin begin, IteratorEnd end);
+    static Array<std::pair<T, uint>> count(IteratorBegin _begin, IteratorEnd _end);
     
     /*
+     * Returns the iterator to the first element of the `Counter` class.
      */
     iterator begin() noexcept
     {
         update();
-        return iterator(_current.begin());
+        return iterator(_current.cbegin(), _current.cend());
     }
     
     /*
+     * Returns the iterator to the end of the `Counter` class. 
      */
     iterator end() const noexcept
     {
-        return iterator(_current.end());
+        return iterator(_current.cend(), _current.cend());
     }
     
 };
 
 /*
+ * Constructor.
+ * @param _begin - the iterator to the beginning of the sequence.
+ * @param _end - the iterator to the end of the sequence. 
  */
 template<typename T>
 template<typename IteratorBegin, typename IteratorEnd>
@@ -239,6 +313,7 @@ Counter<T>::Counter(IteratorBegin _begin, IteratorEnd _end) :
 }
 
 /*
+ * Destructor.
  */
 template<typename T>
 Counter<T>::~Counter()
@@ -246,6 +321,13 @@ Counter<T>::~Counter()
 }
 
 /*
+ * The static `map` function.
+ * Gets two iterators on the sequence.
+ * Countes number of eleemnts in the sequence and 
+ * sorts it by the greater number of elements.
+ * Return `std::map<T, int>`: T - element, int - number of this element.
+ * For example: ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c') 
+ * -> {'a': 4, 'c': 3, 'b': 2, 'd': 1}.
  */
 template<typename T>
 template<typename IteratorBegin, typename IteratorEnd>
@@ -258,6 +340,21 @@ std::map<T, uint> Counter<T>::map(IteratorBegin _begin, IteratorEnd _end)
     return result;
 }
 
+/*
+ * The static `count` function.
+ * Counts number of elements in the sequence and
+ * sorts it by the greater number of elements.
+ * Return your array of pairs.
+ * Your array must supported by functions `std::transform` and `std::inserter`.
+ * For example: sequence - ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c')
+ * ```
+ * std::vector<Counter<std::string>::pair_t> vec = Counter<std::string>::count<std::vector>(begin(...), end(...));
+ * or use just 
+ * auto vec = Counter<std::string>::count<std::vector>(begin(...), end(...));
+ * or use another container
+ * std::set<Counter<std::string>::pair_t set = Counter<std::string>::count<std::set>(begin(...), end(...));
+ * ```
+ */
 template<typename T>
 template<template <class ... > class Array,
         typename IteratorBegin, 
@@ -285,8 +382,10 @@ Array<std::pair<T, uint>> Counter<T>::count(IteratorBegin _begin, IteratorEnd _e
     
     return result;
 }
-    
+
 /*
+ * The private `update` function.
+ * Updates the vector of pairs and sorts it.
  */
 template<typename T>
 auto Counter<T>::count(int& n) -> std::pair<Counter<T>::vc_it, Counter<T>::vc_it>
@@ -314,6 +413,15 @@ void Counter<T>::update()
 }
 
 /*
+ * The `most_common` function.
+ * Counts number of elements in the sequence.
+ * Return vector of pairs. The pairs contains two 
+ * values: (the element in the sequence, the number of such elements).
+ * For example: sequence - ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c') 
+ * -> {('a', 4), ('c', 3), ('b', 2), ('d', 1)}.
+ * The vector of pairs is sorted by the greater number of elements.
+ * If you want to get some number of elements, you must pass a 
+ * number as the first argument.
  */
 template<typename T>
 std::vector<std::pair<T, uint>> Counter<T>::most_common(int n)
@@ -323,6 +431,15 @@ std::vector<std::pair<T, uint>> Counter<T>::most_common(int n)
 }
 
 /*
+ * The alternative `most_common` function.
+ * Counts number of elements in the sequence and
+ * sorts it by the greater number of elements.
+ * After it, this function writes result in the array, 
+ * which was passed as the first argument, but this function
+ * writes only elements in the sequence, without number of elements.
+ * For example: sequence - ('a', 'b', 'c', 'a', 'c', 'd', 'a', 'a', 'b', 'c') 
+ * -> {'a', 'c', 'b', 'd'}.
+ * Your array must have a type. For example: vector<T>.
  */
 template<typename T>
 template<typename Array>
@@ -337,6 +454,7 @@ void Counter<T>::most_common(Array& list, int n)
 }
 
 /*
+ * Update the `Counter` class
  */
 template<typename T>
 template<typename IteratorBegin, typename IteratorEnd>
@@ -345,6 +463,24 @@ void Counter<T>::update(IteratorBegin _begin, IteratorEnd _end)
     for (auto it = _begin; it != _end; it++) {
         _elements[*it]++;
     }
+}
+
+/*
+ * The overloaded `<<` operator for the `Counter` class.
+ * Prints all the elements in the format: `{(a, 10), (b, 8) ... (h, 1)}`.
+ * Returns ostream.
+ */
+template<typename Type>
+std::ostream& operator<<(std::ostream& stream, Counter<Type>& c)
+{
+    stream << "{";
+    for(auto it = c.begin(); it != c.end(); ++it) {
+        stream << "(" << it->first << ", " << it->second << "), ";
+    }
+    stream << "\b\b";
+    stream << "";
+    stream << "}";
+    return stream;
 }
 
 
