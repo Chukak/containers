a4fba885782941f30cc7e4e16e33e8537e1ff31c
Chukak <chukak12@gmail.com>
2019-06-02 01:02:34 +0300

    removed unused options

--- astyler.sh
+++ astyler.sh
@@ -1,6 +1,4 @@
-astyle --indent=tab=4 --indent-namespaces \
-       --indent-preproc-block --indent-preproc-define --indent-col1-comments \
+astyle --indent=tab=4 --indent-col1-comments \
        --pad-oper --pad-comma --pad-header --add-braces --align-pointer=middle \
-       --align-reference=type --max-code-length=160 --close-templates \
-       --attach-return-type --attach-return-type-decl \
-       --recursive ./src/*.cpp,*.h --recursive ./tests/*.cpp,*h --recursive ./tests/performance/*.cpp,*.h
+       --align-reference=type --close-templates \
+       --recursive ./src/*.cpp,*h --recursive ./include/*.cpp,*.h --recursive ./tests/*.cpp,*h --recursive ./tests/performance/*.cpp,*.h
